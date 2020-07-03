02a257c9b8d9efb2e696c4220b681ae11633b769
NaoyaTsudome <noy.dmt1021@gmail.com>
2020-07-03 16:02:38 +0900

    レポジトリにコメントを付与

--- forum/src/main/java/com/example/demo/entity/Report.java
+++ forum/src/main/java/com/example/demo/entity/Report.java
@@ -17,20 +17,24 @@ public class Report {
 	private int id;
 
 	@Column
-	private String content;
+	private String content; // 投稿内容
 
+	// 投稿IDを取得
 	public int getId() {
 		return id;
 	}
 
+	// 投稿IDを格納
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	// 投稿内容を取得
 	public String getContent() {
 		return content;
 	}
 
+	// 投稿内容を格納
 	public void setContent(String content) {
 		this.content = content;
 	}
