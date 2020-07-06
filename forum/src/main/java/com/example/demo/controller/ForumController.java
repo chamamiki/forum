fbab1be9bc6cccc0b6faa134a13668289292054b
Ossonyan <osone.yuta@gmail.com>
2020-07-06 17:12:53 +0900

    クラス名の頭文字を大文字に修正

--- forum/src/main/java/com/example/demo/controller/ForumController.java
+++ forum/src/main/java/com/example/demo/controller/ForumController.java
@@ -14,7 +14,7 @@ import com.example.demo.entity.Report;
 import com.example.demo.service.ReportService;
 
 @Controller
-public class forumController {
+public class ForumController {
 	@Autowired
 	ReportService reportService;
 
